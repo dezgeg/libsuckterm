c6d472684f4023af2ca50eec0b2998f2126a0c5f
Tuomas Tynkkynen <dezgeg@gmail.com>
2013-10-23 01:12:02 +0300

    Extract X event handling.

--- st.c
+++ st.c
@@ -350,15 +350,16 @@ static void xresize(int, int);
 static void expose(XEvent *);
 static void visibility(XEvent *);
 static void unmap(XEvent *);
-static char *kmap(KeySym, uint);
 static void kpress(XEvent *);
 static void cmessage(XEvent *);
-static void cresize(int, int);
 static void resize(XEvent *);
 static void focus(XEvent *);
 static void brelease(XEvent *);
 static void bpress(XEvent *);
 static void bmotion(XEvent *);
+
+static char *kmap(KeySym, uint);
+static void cresize(int, int);
 
 // helpers.c
 static int utf8decode(char *, long *);
@@ -503,27 +504,6 @@ mousereport(XEvent *e) {
 	}
 
 	ttywrite(buf, len);
-}
-
-void
-bpress(XEvent *e) {
-	if(IS_SET(MODE_MOUSE)) {
-		mousereport(e);
-	}
-}
-
-void
-brelease(XEvent *e) {
-	if(IS_SET(MODE_MOUSE)) {
-		mousereport(e);
-	}
-}
-
-void
-bmotion(XEvent *e) {
-	if(IS_SET(MODE_MOUSE)) {
-		mousereport(e);
-	}
 }
 
 #include "pty.c"
@@ -2575,33 +2555,7 @@ drawregion(int x1, int y1, int x2, int y2) {
 	xdrawcursor();
 }
 
-void
-expose(XEvent *ev) {
-	XExposeEvent *e = &ev->xexpose;
-
-	if(xw.state & WIN_REDRAW) {
-		if(!e->count)
-			xw.state &= ~WIN_REDRAW;
-	}
-	redraw(0);
-}
-
-void
-visibility(XEvent *ev) {
-	XVisibilityEvent *e = &ev->xvisibility;
-
-	if(e->state == VisibilityFullyObscured) {
-		xw.state &= ~WIN_VISIBLE;
-	} else if(!(xw.state & WIN_VISIBLE)) {
-		/* need a full redraw for next Expose, not just a buf copy */
-		xw.state |= WIN_VISIBLE | WIN_REDRAW;
-	}
-}
-
-void
-unmap(XEvent *ev) {
-	xw.state &= ~WIN_VISIBLE;
-}
+#include "xevent.c"
 
 void
 xsetpointermotion(int set) {
@@ -2616,27 +2570,6 @@ xseturgency(int add) {
 	h->flags = add ? (h->flags | XUrgencyHint) : (h->flags & ~XUrgencyHint);
 	XSetWMHints(xw.dpy, xw.win, h);
 	XFree(h);
-}
-
-void
-focus(XEvent *ev) {
-	XFocusChangeEvent *e = &ev->xfocus;
-
-	if(e->mode == NotifyGrab)
-		return;
-
-	if(ev->type == FocusIn) {
-		XSetICFocus(xw.xic);
-		xw.state |= WIN_FOCUSED;
-		xseturgency(0);
-		if(IS_SET(MODE_FOCUS))
-			ttywrite("\033[I", 3);
-	} else {
-		XUnsetICFocus(xw.xic);
-		xw.state &= ~WIN_FOCUSED;
-		if(IS_SET(MODE_FOCUS))
-			ttywrite("\033[O", 3);
-	}
 }
 
 static inline bool
@@ -2699,66 +2632,6 @@ kmap(KeySym k, uint state) {
 }
 
 void
-kpress(XEvent *ev) {
-	XKeyEvent *e = &ev->xkey;
-	KeySym ksym;
-	char buf[32], *customkey;
-	int len;
-	long c;
-	Status status;
-
-	if(IS_SET(MODE_KBDLOCK))
-		return;
-
-	len = XmbLookupString(xw.xic, e, buf, sizeof buf, &ksym, &status);
-	e->state &= ~Mod2Mask;
-
-	/* 2. custom keys from config.h */
-	if((customkey = kmap(ksym, e->state))) {
-		ttysend(customkey, strlen(customkey));
-		return;
-	}
-
-	/* 3. composed string from input method */
-	if(len == 0)
-		return;
-	if(len == 1 && e->state & Mod1Mask) {
-		if(IS_SET(MODE_8BIT)) {
-			if(*buf < 0177) {
-				c = *buf | 0x80;
-				len = utf8encode(&c, buf);
-			}
-		} else {
-			buf[1] = buf[0];
-			buf[0] = '\033';
-			len = 2;
-		}
-	}
-	ttysend(buf, len);
-}
-
-
-void
-cmessage(XEvent *e) {
-	/*
-	 * See xembed specs
-	 *  http://standards.freedesktop.org/xembed-spec/xembed-spec-latest.html
-	 */
-	if(e->xclient.message_type == xw.xembed && e->xclient.format == 32) {
-		if(e->xclient.data.l[1] == XEMBED_FOCUS_IN) {
-			xw.state |= WIN_FOCUSED;
-			xseturgency(0);
-		} else if(e->xclient.data.l[1] == XEMBED_FOCUS_OUT) {
-			xw.state &= ~WIN_FOCUSED;
-		}
-	} else if(e->xclient.data.l[0] == xw.wmdeletewin) {
-		/* Send SIGHUP to shell */
-		kill(pid, SIGHUP);
-		exit(EXIT_SUCCESS);
-	}
-}
-
-void
 cresize(int width, int height) {
 	int col, row;
 
@@ -2773,14 +2646,6 @@ cresize(int width, int height) {
 	tresize(col, row);
 	xresize(col, row);
 	ttyresize();
-}
-
-void
-resize(XEvent *e) {
-	if(e->xconfigure.width == xw.w && e->xconfigure.height == xw.h)
-		return;
-
-	cresize(e->xconfigure.width, e->xconfigure.height);
 }
 
 void
---
+++ xevent.c
@@ -0,0 +1,138 @@
+void
+kpress(XEvent *ev) {
+	XKeyEvent *e = &ev->xkey;
+	KeySym ksym;
+	char buf[32], *customkey;
+	int len;
+	long c;
+	Status status;
+
+	if(IS_SET(MODE_KBDLOCK))
+		return;
+
+	len = XmbLookupString(xw.xic, e, buf, sizeof buf, &ksym, &status);
+	e->state &= ~Mod2Mask;
+
+	/* 2. custom keys from config.h */
+	if((customkey = kmap(ksym, e->state))) {
+		ttysend(customkey, strlen(customkey));
+		return;
+	}
+
+	/* 3. composed string from input method */
+	if(len == 0)
+		return;
+	if(len == 1 && e->state & Mod1Mask) {
+		if(IS_SET(MODE_8BIT)) {
+			if(*buf < 0177) {
+				c = *buf | 0x80;
+				len = utf8encode(&c, buf);
+			}
+		} else {
+			buf[1] = buf[0];
+			buf[0] = '\033';
+			len = 2;
+		}
+	}
+	ttysend(buf, len);
+}
+
+
+void
+cmessage(XEvent *e) {
+	/*
+	 * See xembed specs
+	 *  http://standards.freedesktop.org/xembed-spec/xembed-spec-latest.html
+	 */
+	if(e->xclient.message_type == xw.xembed && e->xclient.format == 32) {
+		if(e->xclient.data.l[1] == XEMBED_FOCUS_IN) {
+			xw.state |= WIN_FOCUSED;
+			xseturgency(0);
+		} else if(e->xclient.data.l[1] == XEMBED_FOCUS_OUT) {
+			xw.state &= ~WIN_FOCUSED;
+		}
+	} else if(e->xclient.data.l[0] == xw.wmdeletewin) {
+		/* Send SIGHUP to shell */
+		kill(pid, SIGHUP);
+		exit(EXIT_SUCCESS);
+	}
+}
+
+void
+resize(XEvent *e) {
+	if(e->xconfigure.width == xw.w && e->xconfigure.height == xw.h)
+		return;
+
+	cresize(e->xconfigure.width, e->xconfigure.height);
+}
+
+void
+expose(XEvent *ev) {
+	XExposeEvent *e = &ev->xexpose;
+
+	if(xw.state & WIN_REDRAW) {
+		if(!e->count)
+			xw.state &= ~WIN_REDRAW;
+	}
+	redraw(0);
+}
+
+void
+visibility(XEvent *ev) {
+	XVisibilityEvent *e = &ev->xvisibility;
+
+	if(e->state == VisibilityFullyObscured) {
+		xw.state &= ~WIN_VISIBLE;
+	} else if(!(xw.state & WIN_VISIBLE)) {
+		/* need a full redraw for next Expose, not just a buf copy */
+		xw.state |= WIN_VISIBLE | WIN_REDRAW;
+	}
+}
+
+void
+unmap(XEvent *ev) {
+	xw.state &= ~WIN_VISIBLE;
+}
+
+void
+focus(XEvent *ev) {
+	XFocusChangeEvent *e = &ev->xfocus;
+
+	if(e->mode == NotifyGrab)
+		return;
+
+	if(ev->type == FocusIn) {
+		XSetICFocus(xw.xic);
+		xw.state |= WIN_FOCUSED;
+		xseturgency(0);
+		if(IS_SET(MODE_FOCUS))
+			ttywrite("\033[I", 3);
+	} else {
+		XUnsetICFocus(xw.xic);
+		xw.state &= ~WIN_FOCUSED;
+		if(IS_SET(MODE_FOCUS))
+			ttywrite("\033[O", 3);
+	}
+}
+
+void
+bpress(XEvent *e) {
+	if(IS_SET(MODE_MOUSE)) {
+		mousereport(e);
+	}
+}
+
+void
+brelease(XEvent *e) {
+	if(IS_SET(MODE_MOUSE)) {
+		mousereport(e);
+	}
+}
+
+void
+bmotion(XEvent *e) {
+	if(IS_SET(MODE_MOUSE)) {
+		mousereport(e);
+	}
+}
+
